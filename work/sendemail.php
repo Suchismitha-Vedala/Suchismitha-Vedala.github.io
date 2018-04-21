f9db73bad7e68f24eb3ca2c83c97d7a4034538a5
Suchismitha <svedala@github.com>
2018-04-21 15:29:36 -0500

    email post changed

--- work/sendemail.php
+++ work/sendemail.php
@@ -1,81 +1,34 @@
+
 <?php
-   
 
-    session_cache_limiter( 'nocache' );
-    header( 'Expires: ' . gmdate( 'r', 0 ) );
-    header( 'Content-type: application/json' );
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "v.suchismitha1994@gmail.com";
+    $email_subject = "From Website";
+ 
+ 
+ 
+     
+ 
+    $name = $_POST['name']; // required// required
+    $email_from = $_POST['email']; // required
+    $subject = $_POST['subject']; // not required
+    $message = $_POST['message']; // required
+ 
+    $email_body = "You have received a new message. "
+                    " Here are the details:\n Name: $name \n ".
+                    "Email: $email_from\n Subject: $subject \n Message \n $message";
 
+    $headers = "From: $to\n";
+    $headers .= "Reply-To: $email_from";
+    mail($to,$email_subject,$email_body,$headers);
 
-    $to         = 'v.suchismitha1994@gmail.com';  // put your email here
-
-    //$email_template = 'simple.html';
-
-    $subject    = strip_tags($_POST['subject']);
-    $email       = strip_tags($_POST['email']);
-    $phone      = strip_tags($_POST['phone']);
-    $name       = strip_tags($_POST['name']);
-    $message    = nl2br( htmlspecialchars($_POST['message'], ENT_QUOTES) );
-    $result     = array();
-
-    $formcontent ="From: $name \n Message: $message";
-    $mailheader = "From: $email \r\n";
-
-
-    if(empty($name)){
-
-        $result = array( 'response' => 'error', 'empty'=>'name', 'message'=>'<strong>Error!</strong>&nbsp; Name is empty.' );
-        echo json_encode($result );
-        die;
-    } 
-
-    if(empty($email)){
-
-        $result = array( 'response' => 'error', 'empty'=>'email', 'message'=>'<strong>Error!</strong>&nbsp; Email is empty.' );
-        echo json_encode($result );
-        die;
-    } 
-
-    if(empty($message)){
-
-         $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Error!</strong>&nbsp; Message body is empty.' );
-         echo json_encode($result );
-         die;
-    }
-    
-
-
-    $headers  = "From: " . $name . ' <' . $email . '>' . "\r\n";
-    $headers .= "Reply-To: ". $email . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-
-    $templateTags =  array(
-        '{{subject}}' => $subject,
-        '{{email}}'=>$email,
-        '{{message}}'=>$message,
-        '{{name}}'=>$name,
-        '{{phone}}'=>$phone
-        );
-
-    mail($to, $subject, $formcontent, $mailheader) or die("Error!");
-    echo "Thank You!";
-    /*
-    $templateContents = file_get_contents( dirname(__FILE__) . '/email-templates/'.$email_template);
-
-    $contents =  strtr($templateContents, $templateTags);
-
-    if ( mail( $to, $subject, $contents, $headers ) ) {
-        $result = array( 'response' => 'success', 'message'=>'<strong>Thank You!</strong>&nbsp; Your email has been delivered.' );
-    } else {
-        $result = array( 'response' => 'error', 'message'=>'<strong>Error!</strong>&nbsp; Cann\'t Send Mail.'  );
-    }
-
-    echo json_encode( $result );
-
-    die;*/
-
-
-
+ 
+     
 
 ?>
+ 
+<!-- include your own success html here -->
+
+
+ 
